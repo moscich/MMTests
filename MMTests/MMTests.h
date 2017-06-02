6d0012a2411c3a34d533f24c0de6cb34bf93d35d
Marek Mościchowski <marek.moscichowski@miquido.com>
2017-06-02 20:10:15 +0200

    Initial Commit

---
+++ MMTests/MMTests.h
@@ -0,0 +1,19 @@
+//
+//  MMTests.h
+//  MMTests
+//
+//  Created by Marek Mościchowski on 02.06.2017.
+//  Copyright © 2017 Marek Mościchowski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MMTests.
+FOUNDATION_EXPORT double MMTestsVersionNumber;
+
+//! Project version string for MMTests.
+FOUNDATION_EXPORT const unsigned char MMTestsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MMTests/PublicHeader.h>
+
+
