14e41e6762eeaeaafc086748f8a00e2bf01d0063
Marek Mościchowski <marek.moscichowski@miquido.com>
2017-06-12 22:21:48 +0200

    Added Clipboard.

--- Play.playground/Contents.swift
+++ Play.playground/Contents.swift
@@ -8,10 +8,3 @@ let runner = MMTestRunner()
 let array = runner.runTests() as! [MMTestResult]
 
 let summary = Helper.testSummary(array)
-
-let displayer = MMDisplayer()
-displayer.send(summary.passed, failed: summary.failed) {
-  PlaygroundPage.current.finishExecution()
-}
-
-PlaygroundPage.current.needsIndefiniteExecution = true
