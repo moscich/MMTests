d20da3e55e3117e97b7587973f210c9ea6297303
Marek Mościchowski <marek.moscichowski@miquido.com>
2017-07-28 22:57:28 +0200

    almost there

--- XCTestPlayground.playground/Contents.swift
+++ XCTestPlayground.playground/Contents.swift
@@ -2,49 +2,6 @@
 
 import UIKit
 import XCTest
+import MMTests
 import UI_Framework
-
-class MMTestDemoTests: XCTestCase {
-  
-  func testEmptyString() {
-    let vm = ViewModelImpl()
-    let ctrl = ViewController(viewModel: vm)
-    ctrl.loadViewIfNeeded()
-    XCTAssertEqual(ctrl.label.text!, "")
-  }
-}
-
-MMTestDemoTests.defaultTestSuite().run()
-
-//import UI_Framework
-//import XCTest
-//import MMTests
-//import PlaygroundSupport
-//
-//class ViewControllerTests: XCTestCase {
-//  func testEmpty() {
-//    
-//    let vm = ViewModelImpl()
-//    let ctrl = ViewController(viewModel: vm)
-//    ctrl.loadViewIfNeeded()
-//    XCTAssertEqual(ctrl.label.text!, "")
-//  }
-//}
-//
-//class TestObserver: NSObject, XCTestObservation {
-//
-//  func testCase(_ testCase: XCTestCase,
-//                didFailWithDescription description: String,
-//                inFile filePath: String?,
-//                atLine lineNumber: UInt) {
-//    let displayer = MMDisplayer()
-//    displayer.send("F\(description)") {
-//      PlaygroundPage.current.finishExecution()
-//    }
-//  }
-//}
-//
-//let testObserver = TestObserver()
-//XCTestObservationCenter.shared().addTestObserver(testObserver)
-//
-
+import PlaygroundSupport
